e4453518a719d9146ee3ea692339425c8fea6a42
bulasuo <bulasuo@foxmail.com>
2016-09-18 15:28:43 +0800

    9.18 ++

--- Blink/app/src/main/java/com/hotsand/blink/service/XService.java
+++ Blink/app/src/main/java/com/hotsand/blink/service/XService.java
@@ -1,6 +1,7 @@
 package com.hotsand.blink.service;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
@@ -16,12 +17,13 @@ import java.util.ArrayList;
 /**
  *@author   abu   2016/9/8   11:35
  */
-public class XService extends Service implements OnSocketChangeListener{
+public class XService extends Service {
+    private static Context appContext;
     private static Socket socket;
     public static InputThread in;
     public static OutputThread out;
     public static ArrayList<TranProtocol> tranProtocolList = new ArrayList<>();//存放message的待发送队列
-    private int reConnectCount = 0;//尝试重新连接的计数器
+    private static int reConnectCount = 0;//尝试重新连接的计数器
     public static final int TIME_OUT = 1000;//连接的超时时间
     public static final int RECONNECT_DELAY = 1000;//尝试重连的延迟时间
 
@@ -29,7 +31,7 @@ public class XService extends Service implements OnSocketChangeListener{
     public XService() {
     }
 
-    public void sendMessage(TranProtocol tranProtocol){
+    public static void sendMessage(TranProtocol tranProtocol){
         synchronized (XService.tranProtocolList) {
             tranProtocolList.add(tranProtocol);
             if(initSocket())
@@ -40,6 +42,7 @@ public class XService extends Service implements OnSocketChangeListener{
     @Override
     public void onCreate() {
         super.onCreate();
+        appContext = getApplicationContext();
         System.out.println("Service_onCreate");
     }
 
@@ -63,13 +66,13 @@ public class XService extends Service implements OnSocketChangeListener{
         return null;
     }
 
-    private boolean initSocket() {
+    private static boolean initSocket() {
         synchronized (XService.tranProtocolList) {
             if (socket != null && out != null)
                 return true;
             if (socket == null) {
                 socket = new Socket();
-                new InitSocketThread(this).start();
+                new InitSocketThread(mOnSocketChangeListener).start();
                 return false;
             }
             return false;
@@ -92,48 +95,7 @@ public class XService extends Service implements OnSocketChangeListener{
         }
     }
 
-    @Override
-    public void onSocketConnect() {
-        synchronized (XService.tranProtocolList) {
-            System.out.println("XService:onSocketConnect");
-            reConnectCount = 0;
-        }
-    }
-
-    /**
-     * 连接异常的情况下,最多尝试5次重新连接,这里应该与连接成功后断开区分开
-     *@author   abu   2016/9/10   11:01
-     */
-    @Override
-    public void onSocketUnableConnect() {
-        synchronized (XService.tranProtocolList) {
-            System.out.println("XService:onSocketConnectDisable");
-            if (reConnectCount == -1)
-                return;
-            reConnectCount++;
-            if (reConnectCount < 5) {
-                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        initSocket();
-                    }
-                }, RECONNECT_DELAY);
-            } else {
-                reConnectCount = -1;
-                Toast.makeText(getApplicationContext(), "Unable to connect to the server !", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @Override
-    public void onSocketDisConnect() {
-        synchronized (XService.tranProtocolList) {
-            System.out.println("XService:onSocketDisConnect");
-            initSocket();
-        }
-    }
-
-    private class InitSocketThread extends Thread {
+    private static class InitSocketThread extends Thread {
         private OnSocketChangeListener onSocketChangeListener;
 
         public InitSocketThread(OnSocketChangeListener onSocketChangeListener){
@@ -161,4 +123,49 @@ public class XService extends Service implements OnSocketChangeListener{
             }
         }
     }
+
+    public static OnSocketChangeListener mOnSocketChangeListener = new OnSocketChangeListener(){
+
+        @Override
+        public void onSocketConnect() {
+            synchronized (XService.tranProtocolList) {
+                System.out.println("XService:onSocketConnect");
+                reConnectCount = 0;
+            }
+        }
+
+        /**
+         * 连接异常的情况下,最多尝试5次重新连接,这里应该与连接成功后断开区分开
+         *@author   abu   2016/9/10   11:01
+         */
+        @Override
+        public void onSocketUnableConnect() {
+            synchronized (XService.tranProtocolList) {
+                System.out.println("XService:onSocketConnectDisable");
+                if (reConnectCount == -1)
+                    return;
+                reConnectCount++;
+                if (reConnectCount < 5) {
+                    new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            initSocket();
+                        }
+                    }, RECONNECT_DELAY);
+                } else {
+                    reConnectCount = -1;
+                    if(appContext != null)
+                        Toast.makeText(appContext, "Unable to connect to the server !", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+        @Override
+        public void onSocketDisConnect() {
+            synchronized (XService.tranProtocolList) {
+                System.out.println("XService:onSocketDisConnect");
+                initSocket();
+            }
+        }
+    };
 }
